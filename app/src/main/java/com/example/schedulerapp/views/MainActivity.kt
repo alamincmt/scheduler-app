be20a3a024ea51f701dfb3654ab432f8953865e4
Al Amin <alamin@repto.co>
2022-01-05 08:23:38 +0600

    Necessary libraries added.

--- app/src/main/java/com/example/schedulerapp/views/MainActivity.kt
+++ app/src/main/java/com/example/schedulerapp/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.schedulerapp
+package com.example.schedulerapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.schedulerapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
